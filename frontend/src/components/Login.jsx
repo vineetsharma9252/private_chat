b83f88d2302f97d91bb5b9208862b4b11f908eb9
vineet <Mastergenos228@gmail.com>
2025-06-21 08:52:32 +0530

    Initial commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,7 +20,7 @@ function Login(props) {
 
     try {
       const response = await axios.post(
-        "https://private-chat-joib.vercel.app/login",
+        "https://private-chat-3.onrender.com//login",
         user
       );
 
